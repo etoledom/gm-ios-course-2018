87b4c2c334a434db53b2a74f7fb789ee87482d27
etoledom <etoledom@icloud.com>
2018-09-21 16:36:38 -0300

    BookController: Connected views to outlets

--- onit/onit/BookViewController.swift
+++ onit/onit/BookViewController.swift
@@ -1,20 +1,16 @@
-//
-//  ViewController.swift
-//  onit
-//
-//  Created by Eduardo Toledo on 9/21/18.
-//  Copyright © 2018 GM2018iOS. All rights reserved.
-//
 
 import UIKit
 
 class BookViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
