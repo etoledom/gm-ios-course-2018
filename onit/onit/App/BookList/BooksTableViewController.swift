6e4503df3dd99e2d841cc63e646cabebc247735e
etoledom <etoledom@icloud.com>
2018-09-21 18:18:36 -0300

    Added Book List basic UI

---
+++ onit/onit/App/BookList/BooksTableViewController.swift
@@ -0,0 +1,86 @@
+//
+//  BooksTableViewController.swift
+//  onit
+//
+//  Created by Eduardo Toledo on 9/21/18.
+//  Copyright © 2018 GM2018iOS. All rights reserved.
+//
+
+struct BookViewModel {
+    let title: String
+    let subtitle: String
+}
+
+let datasource = [
+    BookViewModel(title: "\"First Book\"", subtitle: "Author, 2010"),
+    BookViewModel(title: "\"Second Book\"", subtitle: "Author, 2010"),
+    BookViewModel(title: "\"A long book\"", subtitle: "Author, 2010"),
+    BookViewModel(title: "\"No Book\"", subtitle: "Author, 2010")
+]
+
+import UIKit
+
+class BooksTableViewController: UITableViewController {
+
+    let books = datasource
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = getBookCell()
+        let book = books[indexPath.row]
+
+        cell.textLabel?.text = book.title
+        cell.detailTextLabel?.text = book.subtitle
+        cell.accessoryType = .disclosureIndicator
+
+        return cell
+    }
+
+    func getBookCell() -> UITableViewCell {
+        let reuseID = "book_cell"
+        if let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: reuseID) {
+            return dequeuedCell
+        } else {
+            return UITableViewCell(style: .subtitle, reuseIdentifier: reuseID)
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
