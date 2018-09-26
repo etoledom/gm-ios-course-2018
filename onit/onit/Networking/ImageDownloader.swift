d17e860ca0620b9a7fe1894927669279312f2904
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-09-26 13:58:19 +0800

    Add image downloading

---
+++ onit/onit/Networking/ImageDownloader.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+final class ImageDownloader {
+    static func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+}
---
+++ onit/onit/Networking/UIImageView+Network.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+extension UIImageView {
+    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else {
+            return
+
+        }
+
+        downloaded(from: url, contentMode: mode)
+    }
+
+    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        ImageDownloader.getData(from: url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else {
+                    return
+
+            }
+            
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+        }
+    }
+}
